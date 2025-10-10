991180d910efa0bbb2bc1eee573e57bad97eae69
NKrashewsky <140845129+NKrashewsky@users.noreply.github.com>
2025-10-10 20:45:29 +0300

    Add files via upload

---
+++ PM1T01/Mediana.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "");
+    const int n = 5;
+    int  arr[n], mem, i, j;
+    cout << "Введите " << n << " чисел:\n";
+    for (i = 0; i < 5; i++) {
+        cin >> arr[i];     
+    }
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < (n - i - 1); j++) {
+            if (arr[j] > arr[j + 1]) {
+                mem = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = mem;
+            }
+        }
+    }
+    cout << arr[2];
+}
+
---
+++ PM1T01/Mushrooms.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <locale>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int n, k, p;
+
+	cin >> n;
+
+	k = n % 10;
+	p = (n % 100) / 10;
+	if (n == 0)
+		cout << "Мы не собрали грибов в лесу";
+	else
+	{
+		if ((k == 1 && p == 0) || (k == 1 && p != 1))
+			cout << "Мы собрали " << n << " гриб";
+
+		if ((k >= 2 && k <= 4 && p == 0) || (k >= 2 && k <= 4 && p != 1))
+			cout << "Мы собрали " << n << " гриба";
+
+		if ((p == 0 && k >= 5) || (p == 1) || (p > 1 && k >= 5))
+			cout << "Мы собрали " << n << " грибов";
+	}
+}
